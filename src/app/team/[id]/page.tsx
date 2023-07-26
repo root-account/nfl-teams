06dda67c526ad0a2099e1af887c5235e07f6b245
Ephraim <mekgwele@gmail.com>
2023-07-26 14:49:13 +0200

    Update page.tsx

--- src/app/team/[id]/page.tsx
+++ src/app/team/[id]/page.tsx
@@ -5,11 +5,10 @@ import axios from 'axios';
 
 
 const getTeamData = (teamId:number) => axios.get(`https://delivery.chalk247.com/team_report/NFL/${teamId}?api_key=74db8efa2a6db279393b433d97c2bc843f8e32b0`).then(function(response) {
-    const data = response.data    
+    const data = response.data       
     return data;
 }).catch(function(error) {
-  console.log(error.response.data.result);
-  return error.response.data.result;
+  return error.response.data.results;
 });
 
 
@@ -17,8 +16,8 @@ export default async function Team({ params }: { params: { id: number } }) {
 
 
   const teamData = await getTeamData(params?.id);
-
   
+
   return (
     <main className="flex min-h-screen flex-col items-center md:p-24 before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
       
@@ -30,7 +29,7 @@ export default async function Team({ params }: { params: { id: number } }) {
         >
             
           <p className={`m-0 text-sm opacity-50`}>
-            Team data for ID : {params.id}
+            Team data for ID: {params.id}
           </p>
 
           {/* If there is an error from API show it here */}
