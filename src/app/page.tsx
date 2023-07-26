759c022cd450d6fe83b2140c1893fb96756ed30b
Ephraim <mekgwele@gmail.com>
2023-07-26 14:02:00 +0200

    Style button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,6 @@ import {setTeams, setFilterInputs} from "@/redux/teams-slice";
 import { revalidatePath } from 'next/cache';
 
 
-
 let divisionFilterOptions = ['West', 'South', 'North', 'East'];
 
 let count = 0;
@@ -16,23 +15,6 @@ let filterByDivision = "";
 let filterByConference = "";
 let conferenceFilterOptions:any =['National Football Conference', 'American Football Conference' ];
 let teamsData:any = [];
-
-interface Team {
-  name: string;
-  nickname: string;
-  display_name: string;
-  id: string;
-  conference: string;
-  division: string;
-}
-
-interface TeamData {
-  results: {
-    data: {
-      team: Team[];
-    };
-  };
-}
 
 
 const getTeamsData  = axios.get('https://delivery.chalk247.com/team_list/NFL.JSON?api_key=74db8efa2a6db279393b433d97c2bc843f8e32b0').then(function(response) {
@@ -89,9 +71,6 @@ export default async function Home() {
     filterByConference = String(formData.get('filterByConference'))
 
     filterTeams(filterByDivision, filterByConference);
-
-    console.log('NEW DATA');
-    console.log(teamsData);
     
     revalidatePath("/");
   }
@@ -139,38 +118,39 @@ export default async function Home() {
 
       <div className="mb-15 block w-full sm:mt-20">
 
-      <div className="mb-8 grid w-full px-10 text-center lg:grid-cols-3 gap-5 lg:text-left relative z-[1]">
+      <div className="mb-8 grid w-full px-10 text-center lg:grid-cols-2 gap-5 lg:text-left relative z-[1]">
 
-        <form className='w-50 mb- block' action={handleFormChange}>
+        <form className='w-100 mb- block' action={handleFormChange}>
           <label className='mb-2 block' htmlFor="">Filter By</label>
-          <div className='grid w-full lg:grid-cols-2 gap-5 '>
+
+          <div className='grid w-full lg:grid-cols-3 gap-5 items-center justify-center'>
 
             <div className='w-full '>
-            <label className='block'>Divison</label>
-            <select name="filterByDivision" className='w-full block bg-transparent border border-neutral-700 rounded-lg p-2 outline-0'>
-                <option value="">All</option>
-                {divisionFilterOptions.map((option, index) => (
-                <option key={index} value={option}>
-                    {option}
-                </option>
-                ))}
-            </select>
+              <label className='block'>Divison</label>
+              <select name="filterByDivision" className='w-full block bg-transparent border border-neutral-700 rounded-lg p-2 outline-0'>
+                  <option value="">All</option>
+                  {divisionFilterOptions.map((option, index) => (
+                  <option key={index} value={option}>
+                      {option}
+                  </option>
+                  ))}
+              </select>
             </div>
             
             <div className='w-full '>
-            <label className='block'>Conference</label>
-            <select name="filterByConference" className='w-full block bg-transparent border border-neutral-700 rounded-lg p-2 outline-0'>
-                <option value="">All</option>
-                {conferenceFilterOptions?.map((option:any, index:number) => (
-                <option key={index} value={option}>
-                    {option}
-                </option>
-                ))}
-            </select>
+              <label className='block'>Conference</label>
+              <select name="filterByConference" className='w-full block bg-transparent border border-neutral-700 rounded-lg p-2 outline-0'>
+                  <option value="">All</option>
+                  {conferenceFilterOptions?.map((option:any, index:number) => (
+                  <option key={index} value={option}>
+                      {option}
+                  </option>
+                  ))}
+              </select>
             </div>
    
             <div>
-              <button type='submit'>Filter</button>  
+              <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full' type='submit'>Apply Filter</button>  
             </div>
           </div> 
 
@@ -179,21 +159,7 @@ export default async function Home() {
     
       </div>
 
-        {/* {error !== "" && (
-            <p className="mt-4 mb-8 text-rose-700 w-full px-10">
-                {error}
-            </p>
-        )} */}
     </div>
-
-        
-      {/* <form action={handleFormChange}>
-        <select name="filterByDivision">
-          <option value="division">Division</option>
-          <option value="conference">Conference</option>
-        </select>
-        <button>Go get the data</button>
-      </form> */}
 
 
       <div className="mb-32 grid w-full px-10 text-left lg:mb-0 lg:grid-cols-3 gap-5 relative z-[1]">
